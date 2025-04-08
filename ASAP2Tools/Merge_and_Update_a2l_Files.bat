f32b8f80f5f7acf2e1f6445d663c6db84d0531a5
Stum Huang <shuang@best2e.com>
2025-04-08 23:42:14 +0800

    Update path

--- ASAP2Tools/Merge_and_Update_a2l_Files.bat
+++ ASAP2Tools/Merge_and_Update_a2l_Files.bat
@@ -1,99 +1,88 @@
 @ECHO off
 
-::set ASAP2 Tool-Set Installation Directory
+set MasterDir=01_Master
+
+set SlaveDir=02_Slaves
+
+set MergedDir=03_Merged
+
+set VstPath=C:\Users\shuang\Documents\001_Git_Repository\Thu_IEEB\01_ATI\PCM
+
+:: Set ASAP2 Tool-Set Installation Directory
 set ASAP2ToolInstallDir=C:\Program Files\Vector\ASAP2 Tool-Set 15.0\Bin
 set ASAP2ToolInstallDirString="%ASAP2ToolInstallDir%"
 set MergerExe=ASAP2Merger.exe
 set UpdaterExe=ASAP2Updater.exe
 
-::check Vector merger installation
+:: Check Vector merger installation
 if not exist %ASAP2ToolInstallDirString%\%MergerExe% (
-@ECHO.
-@ECHO !!!!!!!!!!ERROR!!!!!!!!!!
-@ECHO.
-@ECHO %MergerExe% not found at %ASAP2ToolInstallDirString%\! Please check for valid Installation of ASAP2 Tool-Set!
-@ECHO.
-@pause
-exit
+    @ECHO !!!!!!!!!!ERROR!!!!!!!!!!
+    @ECHO %MergerExe% not found at %ASAP2ToolInstallDirString%\! Please check for valid Installation of ASAP2 Tool-Set!
+    @pause
+    exit
 )
 
-::check Vector updater installation
+:: Check Vector updater installation
 if not exist %ASAP2ToolInstallDirString%\%UpdaterExe% (
-@ECHO.
-@ECHO !!!!!!!!!!ERROR!!!!!!!!!!
-@ECHO.
-@ECHO %UpdaterExe% not found at %ASAP2ToolInstallDirString%\! Please check for valid Installation of ASAP2 Tool-Set!
-@ECHO.
-@pause
-exit
+    @ECHO !!!!!!!!!!ERROR!!!!!!!!!!
+    @ECHO %UpdaterExe% not found at %ASAP2ToolInstallDirString%\! Please check for valid Installation of ASAP2 Tool-Set!
+    @pause
+    exit
 )
 
-::set environment variables for directories
-set MasterDir=01_Master
-set SlaveDir=02_Slaves
-set MergedDir=03_Merged
+:: Ensure VstPath directory exists
+if not exist "%VstPath%" (
+    @ECHO !!!!!!!!!!ERROR!!!!!!!!!!
+    @ECHO VstPath directory "%VstPath%" does not exist! Please create it or update the script.
+    @pause
+    exit
+)
 
-::check if master A2L file exists
+:: Check if master A2L file exists
 if not exist %MasterDir%\*.a2l (
-@ECHO.
-@ECHO !!!!!!!!!!ERROR!!!!!!!!!!
-@ECHO.
-@ECHO No A2L file found at .\%MasterDir%\! Please insert supplier A2L file!
-@ECHO.
-@pause
-exit
+    @ECHO !!!!!!!!!!ERROR!!!!!!!!!!
+    @ECHO No A2L file found at .\%MasterDir%\! Please insert supplier A2L file!
+    @pause
+    exit
 )
 
-::check that max one master A2L file exists
+:: Check that max one master A2L file exists
 set MasterA2LCount=0
 for %%x in (%MasterDir%\*.a2l) do set /a MasterA2LCount+=1
 if %MasterA2LCount% GTR 1 (
-@ECHO.
-@ECHO !!!!!!!!!!ERROR!!!!!!!!!!
-@ECHO.
-@ECHO Too many A2L files found at .\%MasterDir%\! Please insert only one supplier A2L file!
-@ECHO.
-@pause
-exit
+    @ECHO !!!!!!!!!!ERROR!!!!!!!!!!
+    @ECHO Too many A2L files found at .\%MasterDir%\! Please insert only one supplier A2L file!
+    @pause
+    exit
 )
 
-::check if master out file exists
+:: Check if master out file exists
 if not exist %MasterDir%\*.out (
-@ECHO.
-@ECHO !!!!!!!!!!ERROR!!!!!!!!!!
-@ECHO.
-@ECHO No out file found at .\%MasterDir%\! Please insert supplier out file!
-@ECHO.
-@pause
-exit
+    @ECHO !!!!!!!!!!ERROR!!!!!!!!!!
+    @ECHO No out file found at .\%MasterDir%\! Please insert supplier out file!
+    @pause
+    exit
 )
 
-::check that max one master out file exists
+:: Check that max one master out file exists
 set MasterMapCount=0
 for %%x in (%MasterDir%\*.out) do set /a MasterMapCount+=1
 if %MasterMapCount% GTR 1 (
-@ECHO.
-@ECHO !!!!!!!!!!ERROR!!!!!!!!!!
-@ECHO.
-@ECHO Too many out files found at .\%MasterDir%\! Please insert only one supplier out file!
-@ECHO.
-@pause
-exit
+    @ECHO !!!!!!!!!!ERROR!!!!!!!!!!
+    @ECHO Too many out files found at .\%MasterDir%\! Please insert only one supplier out file!
+    @pause
+    exit
 )
 
-::check if at least one slave A2L file exists
+:: Check if at least one slave A2L file exists
 if not exist %SlaveDir%\*.a2l (
-@ECHO.
-@ECHO !!!!!!!!!!ERROR!!!!!!!!!!
-@ECHO.
-@ECHO No A2L file found at .\%SlaveDir%\! Please insert at least one FIH A2L file!
-@ECHO.
-@pause
-exit
+    @ECHO !!!!!!!!!!ERROR!!!!!!!!!!
+    @ECHO No A2L file found at .\%SlaveDir%\! Please insert at least one FIH A2L file!
+    @pause
+    exit
 )
 
-
-::print input files
+:: Print input files
 @ECHO.
 @ECHO Supplier A2L file:
 for /r %%f in (%MasterDir%\*.a2l) do @ECHO %%~nxf
@@ -105,36 +94,40 @@ for /r %%f in (%MasterDir%\*.out) do @ECHO %%~nxf
 for /r %%f in (%SlaveDir%\*.a2l) do @ECHO %%~nxf
 @ECHO.
 
-
-::clean output folder
+:: Clean output folder
 erase /Q .\%MergedDir%\*
 
-::initialize output folder with renamed supplier a2l file
+:: Initialize output folder with renamed supplier A2L file
 copy .\%MasterDir%\*.a2l .\%MergedDir%\
 cd %MergedDir%
-for /f "delims=." %%i in ('dir /b *.a2l') do @rename "%%i.a2l" "%%i_FIH_merged.a2l"
+for /f "delims=." %%i in ('dir /b *.a2l') do @rename "%%i.a2l" "%%i_merged.a2l"
 cd ..
 
-::get renamed output a2l file name
+:: Get renamed output A2L file name
 for %%a in (.\%MergedDir%\*) do set FihMerged=%%~nxa
 
-::merge a2l files
+:: Merge A2L files
 for /r .\%SlaveDir%\ %%a in (*.a2l) do ("%ASAP2ToolInstallDir%\%MergerExe%" -M .\%MergedDir%\%FihMerged% -S %%a -O .\%MergedDir%\%FihMerged%)
 
-::get out file name
+:: Get out file name
 for %%a in (.\%MasterDir%\*.out) do set MapFile=%%~nxa
 
-::update a2l file
+:: Update A2L file
 "%ASAP2ToolInstallDir%\%UpdaterExe%" -I .\%MergedDir%\%FihMerged% -O .\%MergedDir%\%FihMerged% -A .\%MasterDir%\%MapFile% -L .\%MergedDir%\log1.txt -T Updater.ini
 
-:: convert to UTF-8
-sed -i "/ASAP2_VERSION/,$!d" .\%MergedDir%\%FihMerged%
+:: Convert to UTF-8
+if exist .\%MergedDir%\%FihMerged% (
+    sed -i "/ASAP2_VERSION/,$!d" .\%MergedDir%\%FihMerged%
+) else (
+    @ECHO !!!!!!!!!!ERROR!!!!!!!!!!
+    @ECHO Merged A2L file not found! Conversion to UTF-8 failed.
+    @pause
+    exit
+)
 
-::end
-
-::vst build
+:: Build VST file
 @ECHO.
-@ECHO Build Vst File:
+@ECHO Build VST File:
 @ECHO.
 @ECHO ELF H32 File:
 for %%a in (.\%MasterDir%\*.h32) do set H32File=%%~nxa
@@ -151,16 +144,19 @@ for /r %%f in (..\"VST_Tool"\*.vbs) do set VST_VBS=%%~nxf
 @ECHO %VST_VBS%
 
 @ECHO.
+@ECHO A2L File
 for %%a in (.\%MergedDir%\*.a2l) do set A2LFile=%%~nxa
+@ECHO %A2LFile%
+
+@ECHO.
 cd %MergedDir%
-for /f "delims=." %%i in ('dir /b *.a2l') do set VSTFile=%%i.vst
+for /f "delims=." %%i in ('dir /b *.a2l') do set VSTFile=%VstPath%\%%i.vst
+@ECHO %VSTFile%
 cd ..
 
-cscript "%~dp0\..\VST_Tool\%VST_VBS%" "%~dp0\..\VST_Tool\%VST_Tool%" "%~dp0\.\%MergedDir%\%A2LFile%" "%~dp0\.\%MasterDir%\%H32File%" "%~dp0\.\%MergedDir%\%VSTFile%"
+cscript "%~dp0\..\VST_Tool\%VST_VBS%" "%~dp0\..\VST_Tool\%VST_Tool%" "%~dp0\.\%MergedDir%\%A2LFile%" "%~dp0\.\%MasterDir%\%H32File%" "%VSTFile%"
 
-::end
+:: End
 @ECHO.
-@ECHO.
-@ECHO %~dp0\%VSTFile% created.
-@ECHO.
+@ECHO %VSTFile% created.
 @pause
